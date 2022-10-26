35337b182fb10dd051efa0ed8d0ec01459bc72ce
irfanf007 <fauzee.bro@gmail.com>
2022-10-26 13:26:28 +0700

    Big O Notation - Irfan Fauzi

---
+++ src/Exponential.java
@@ -0,0 +1,13 @@
+public class Exponential {
+    public static void main(String args[]){
+        int n = 100;
+        System.out.println(fibonacci(5));
+    }
+    static int fibonacci(int number)
+    {
+        if (number <= 1)
+            return number;
+
+        return fibonacci(number - 1) + fibonacci(number - 2);
+    }
+}
